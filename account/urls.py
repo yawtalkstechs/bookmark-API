d1e3dc57bff2912d4e835dfc4aaace8b8797c35c
yawtalkstechs <yawtalkstechs@gmail.com>
2022-10-10 14:17:36 +0000

    User authentication completed successfully

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,30 @@
-from django.urls import path
-from .views import user_login
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from .views import user_login, dashboard
+
 
 urlpatterns = [
-    path('login/', user_login, name='login'),
+    # path('login/', user_login, name='login'),
+    # path("login/", auth_views.LoginView.as_view(), name="login"),
+    # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+
+    path("", include("django.contrib.auth.urls")),
+    path("", dashboard, name="dashboard"),
+
+    # Password change
+    # path("password-change/", auth_views.PasswordChangeView.as_view(), 
+        # name="password_change"),
+    # path("password-change/done/", auth_views.PasswordChangeDoneView.as_view(), 
+        # name="password_change_done"),
+
+    # Password reset
+    # path("password-reset/", auth_views.PasswordResetView.as_view(), 
+        # name="password_reset"),
+    # path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(), 
+        # name="password_reset_done"),
+    # path("password-reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(),
+        # name="password_reset_confirm"),
+    # path("password-reset/complete/", auth_views.PasswordResetCompleteView.as_view(), 
+        # name="password_reset_complete")
+
 ]
--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
